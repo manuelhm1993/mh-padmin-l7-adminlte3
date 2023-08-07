8a0c424025dd3e5a39da59336f86fda7d8647767
Manuel Henriquez <manuelhm1993@gmail.com>
2023-08-07 15:12:37 -0400

    Configurar las vistas
    Se llama al layout de adminlte sutituyendo el scaffolding por defecto de laravel ui: https://github.com/jeroennoten/Laravel-AdminLTE/wiki/Authentication-Views

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,28 @@
-@extends('layouts.app')
+{{-- Sustituir el layout.app por el layout de adminlte --}}
+@extends('adminlte::page')
+
+@section('title', 'MHenriquez')
+
+@section('content_header')
+    <h1>Tablero</h1>
+@stop
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+    <div class="card">
+        <div class="card-header">
+            <h1 class="card-title">Hola mundo</h1>
+        </div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
+        <div class="card-body">
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugiat odit debitis aliquid nemo, voluptas ipsam ea praesentium pariatur nostrum accusamus voluptates fuga. Minus culpa, provident consequatur laudantium fuga sapiente exercitationem.</p>
         </div>
     </div>
-</div>
-@endsection
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+    <script> console.log('Hi!'); </script>
+@stop
